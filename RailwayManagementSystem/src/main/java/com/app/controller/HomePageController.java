69e2c3de9498f3e805d536228bf36e1e62d80e65
unknown <atharvajadhav007@gmail.com>
2025-06-04 19:59:51 +0530

    javaBackendProjects

---
+++ RailwayManagementSystem/src/main/java/com/app/controller/HomePageController.java
@@ -0,0 +1,24 @@
+package com.app.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/railway")
+public class HomePageController {
+	
+	public HomePageController() {
+		System.out.println("in home page ctor");
+	}
+	
+	@GetMapping("/")
+	public List<Integer> getNumberedList(){
+		System.out.println("in numberList");
+		return Arrays.asList(10,20,30,40);
+	}
+
+}
---
+++ RailwayManagementSystem/src/main/java/com/app/entity/Railway.java
@@ -0,0 +1,27 @@
+package com.app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name="railways")
+public class Railway extends BaseEntity{
+	
+	private String tname;
+	private Category cat;
+	
+
+}
